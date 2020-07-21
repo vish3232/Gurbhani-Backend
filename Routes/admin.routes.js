bf62f6e6c2d719e95ff04b43adb36b9d8bb91513
Vishal Purane <vishalpurane59@gmail.com>
2020-07-21 20:25:29 +0530

    make it better

--- Routes/admin.routes.js
+++ Routes/admin.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const config = require('config');
 const bcrypt = require('bcryptjs');
 
 
