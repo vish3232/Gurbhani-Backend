09e4da4c829f860aaafffcdb609d0255c6366851
Vishal Purane <vishalpurane59@gmail.com>
2020-07-21 19:46:21 +0530

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 8000;
 const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
@@ -23,6 +24,6 @@ app.use("/api",require('./Routes/users.routes'))
 app.use("/api/admin",require('./Routes/admin.routes'))
 app.use("/api",require('./Routes/payment.table'))
 app.use("/api",require('./Routes/notification.router'))
-app.listen(process.env.PORT || 4000, () => {
+app.listen(port, () => {
     console.log('Running on port 4000')
 })
